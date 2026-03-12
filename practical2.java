f35456304745ca7caab787c1d283792c08b0342f
Chetna Magar <chetanamagar2006@gmail.com>
2026-03-12 17:50:13 +0530

    conditional statement

--- practical2.java
+++ practical2.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
 public class practical2 {
     public static void main(String[] args) {
-        // variables
-        int a =10;
-        int b =25;
-        int sum = a + b;
-        int diff = b - a;
-        int multi = a * b;
-        int div = a / b ;
-        
-        System.out.println("the sum of a and b is : " + sum);
-        System.out.println("the difference of a and b is : " + diff);
-        System.out.println("the multiplication of a and b is : " + multi);
-        System.out.println("the division of a and b is : " + div);
+        Scanner sc = new Scanner(System.in);
+        int button = sc. nextInt();
+
+       // if(button == 1) {
+         //   System.out.println("Hello");
+        //} else if(button == 2) {
+          //  System.out.println("Namaste");
+        //} else if(button == 3) {
+          //  System.out.println("Bonjour");
+        //} else {
+          //  System.out.println("Invalid Button");
+       // }
+        //sc.close();
+
+       switch(button) {
+
+        case 1: System.out.println("Hello");
+        break;
+        case 2: System.out.println("Namaste");
+        break;
+        case 3: System.out.println("Bonjour");
+        break;
+        default: System.out.println("Invalid Button");
+
+       }
+       sc.close();
     }
-    
 }
