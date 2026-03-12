8dc7219cbf0466c0b0faa51350d9134c4b6637d3
Chetna Magar <chetanamagar2006@gmail.com>
2026-03-12 17:45:59 +0530

    java

---
+++ practical2.java
@@ -0,0 +1,17 @@
+public class practical2 {
+    public static void main(String[] args) {
+        // variables
+        int a =10;
+        int b =25;
+        int sum = a + b;
+        int diff = b - a;
+        int multi = a * b;
+        int div = a / b ;
+        
+        System.out.println("the sum of a and b is : " + sum);
+        System.out.println("the difference of a and b is : " + diff);
+        System.out.println("the multiplication of a and b is : " + multi);
+        System.out.println("the division of a and b is : " + div);
+    }
+    
+}
