295883d96b17acb7e7e7c1e83c69cfda90ff5338
Chetna Magar <chetanamagar2006@gmail.com>
2026-03-12 17:45:40 +0530

    java

---
+++ practical1.java
@@ -0,0 +1,8 @@
+public class practical1 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
