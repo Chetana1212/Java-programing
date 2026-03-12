3470b4a24aae559f219ae86c93df4f6752c75ddd
Chetna Magar <chetanamagar2006@gmail.com>
2026-03-12 17:49:36 +0530

    conditional statement

--- practical1.java
+++ practical1.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
 public class practical1 {
     public static void main(String[] args) {
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a == b) {
+            System.out.println("Equal");
+        } else if(a > b) {
+            System.out.println("a is Greater");
+        } else {
+            System.out.println("b is Smaller");
+        }
+        sc.close();
     }
+    
 }
